a1c076aa8617be72182fe4e1c0ace977487ba617
Romchik <romankolesov44@gmail.com>
2023-10-22 10:22:25 +0500

    Update DELETE_Roman_Kolesov.sql

--- DELETE_Roman_Kolesov.sql
+++ DELETE_Roman_Kolesov.sql
@@ -8,4 +8,23 @@ WHERE inventory_id IN (SELECT inventory_id FROM inventory WHERE film_id = (SELEC
 DELETE FROM inventory
 WHERE film_id = (SELECT film_id FROM film WHERE film_id = 1001);
 
+DELETE FROM film
+WHERE film_id = 1001;
+
+-- Remove any records related to you (as a customer) from all tables except "Customer" and "Inventory"
+
+DELETE FROM payment WHERE customer_id = 1;
+DELETE FROM rental WHERE customer_id = 1;
+DELETE FROM address WHERE address_id = (SELECT address_id FROM CUSTOMER WHERE customer_id = 1);
+
+
+
+-- Удаление самого customer
+--DELETE FROM CUSTOMER WHERE customer_id = 1;
+
+
+
+
+
+
 
